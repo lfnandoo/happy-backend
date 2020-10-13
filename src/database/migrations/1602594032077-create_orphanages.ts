1a34ad3f56c6e1190b76f062a480480e39b81216
lfnandoo <devfernandoof@gmail.com>
2020-10-13 11:34:39 -0300

    🚀 create orphanage

--- src/database/migrations/1602594032077-create_orphanages.ts
+++ src/database/migrations/1602594032077-create_orphanages.ts
@@ -1,7 +1,6 @@
 import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class createOrphanages1602594032077 implements MigrationInterface {
-  
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(new Table({
       name: 'orphanages',
@@ -11,8 +10,8 @@ export class createOrphanages1602594032077 implements MigrationInterface {
           type: 'integer',
           unsigned: true,
           isPrimary: true,
-          isGenerated:true,
-          generationStrategy: 'increment'
+          isGenerated: true,
+          generationStrategy: 'increment',
         },
         {
           name: 'name',
@@ -32,11 +31,11 @@ export class createOrphanages1602594032077 implements MigrationInterface {
         },
         {
           name: 'about',
-          type: 'text'
+          type: 'text',
         },
         {
-          name: 'instrucionts',
-          type: 'text'
+          name: 'instructions',
+          type: 'text',
         },
         {
           name: 'opening_hours',
@@ -45,14 +44,13 @@ export class createOrphanages1602594032077 implements MigrationInterface {
         {
           name: 'open_on_weekends',
           type: 'boolean',
-          default: 'false'
+          default: false,
         }
       ]
     }))
   }
-  
+
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('orphanages')
+    await queryRunner.dropTable('orphanages');
   }
-  
 }
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { getRepository } from 'typeorm';
+import Orphanage from './models/Orphanages';
 
 import './database/connection';
 
@@ -6,8 +8,32 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/users", (request, response) => {
+app.post("/orphanages", async (request, response) => {
+  const {
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends
+  } = request.body;
 
+  const orphanagesRepository = getRepository(Orphanage);
+
+  const orphanage = orphanagesRepository.create({
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends
+  })
+
+  await orphanagesRepository.save(orphanage);
+
+  return console.log("POSTO");
 });
 
 app.listen(3333);
